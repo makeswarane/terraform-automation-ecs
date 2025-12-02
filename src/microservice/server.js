b7d9f4dd84fd70ec67e58114e8e9636dc5a304ce
MAKESWARAN-E <emakeshwaran1@gmail.com>
2025-12-02 18:25:25 +0530

    deploy

---
+++ src/microservice/server.js
@@ -0,0 +1,12 @@
+// microservice/server.js
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Microservice");
+});
+
+app.listen(port, () => {
+  console.log(`Microservice listening on port ${port}`);
+});

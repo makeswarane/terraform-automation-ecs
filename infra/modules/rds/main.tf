21f18bc2ede5f3d591b797f3bfa083b493054f98
MAKESWARAN-E <emakeshwaran1@gmail.com>
2025-12-02 10:27:25 +0530

    test demo

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -9,9 +9,12 @@ resource "aws_db_instance" "wp" {
   engine                  = "mysql"
   engine_version          = "8.0"
   instance_class          = var.db_instance_class
-  name                    = var.db_name
-  username                = var.db_username
-  password                = var.db_password
+
+  # ✅ Correct argument is db_name (not name)
+  db_name   = var.db_name
+  username  = var.db_username
+  password  = var.db_password
+
   db_subnet_group_name    = aws_db_subnet_group.this.name
   publicly_accessible     = false
   vpc_security_group_ids  = [var.rds_security_group_id]

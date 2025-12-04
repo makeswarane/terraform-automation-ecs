bfa49a1b49a38b3170f88b3399789cffa20626c5
MAKESWARAN-E <emakeshwaran1@gmail.com>
2025-12-04 17:25:39 +0530

    test

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "piedpipers-tf-state"      # your bucket
     key            = "infra/terraform.tfstate"  # path inside bucket
     region         = "ap-south-1"
-    dynamodb_table = "piedpipers-tf-lock"       # or remove if you didn't create it
+    dynamodb_table = "piedpipers-tf-lock"       # or remove if you didn't create it 
     encrypt        = true
   }
 }
